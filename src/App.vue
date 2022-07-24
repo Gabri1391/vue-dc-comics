03ef1e33ab21c452418415f40ca6b007d630b09b
Gabri1391 <chiarellogabriele@gmail.com>
2022-07-24 12:59:40 +0200

    Header done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <BaseHeader />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseHeader from "./components/BaseHeader.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BaseHeader,
   }
+
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  font-family: 'Open Sans', sans-serif;
 }
+
+a{
+  text-decoration: none;
+}
+
+ul li{
+  list-style-type: none;
+}
+
+
+
 </style>
---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,124 @@
+<template>
+    <header>
+        <figure>
+            <img src="../assets/img/dc-logo.png" alt="DC logo">
+        </figure>
+        <ul>
+          <li v-for="link in links" :key="link.id" >
+          <a :class="{active: link.current}" :href="link.url">
+          {{link.text}}</a>
+          </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+ export default {
+    name: 'BaseHeader',
+    data(){
+        return{
+            links:[
+  {
+    id:1,
+    text: 'Characters',
+    url: '#',
+    current: false,
+  },
+  {
+    id:2,
+    text: 'Comics',
+    url: '#',
+    current: true,
+  },
+  {
+    id:3,
+    text: 'Movies',
+    url: '#',
+    current: false,
+  },
+  {
+    id:4,
+    text: 'TV',
+    url: '#',
+    current: false,
+  },
+  {
+    id:5,
+    text: 'Games',
+    url: '#',
+    current: false,
+  },
+  {
+    id:6,
+    text: 'Collectibles',
+    url: '#',
+    current: false,
+  },
+  {
+    id:7,
+    text: 'Videos',
+    url: '#',
+    current: false,
+  },
+  {
+    id:8,
+    text: 'Fans',
+    url: '#',
+    current: false,
+  },
+  {
+    id:9,
+    text: 'News',
+    url: '#',
+    current: false,
+  },
+  {
+    id:10,
+    text: 'Shop',
+    url: '#',
+    current: false,
+  },
+]
+        }
+    }
+
+ }
+
+</script>
+
+<style lang="scss" scoped>
+ header{
+    padding-top: 30px;
+    height: 100px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+ }
+
+img{
+    padding-right: 30px;
+}
+
+ ul{
+    display: flex;
+    justify-content: center;
+    padding-right: 120px;
+    margin: 20px;
+ }
+
+ li{
+    padding: 10px;
+ }
+
+  a{
+    color: black;
+    font-weight: bold;
+    font-size: 0.8rem;
+    padding: 10px;
+  }
+
+  a.active{
+    border-bottom: 1px solid blue;
+  }
+
+</style>
